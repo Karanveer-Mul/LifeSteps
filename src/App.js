3deff341183c0fa9b2a59c0454280c6e51e18f42
Karanveer-Mul <karanveer7171@gmail.com>
2021-01-19 01:12:24 +0530

    changing back to white theme

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import Sidebar from "./Components/layout/sidebar";
+import LandingPage from "./Components/layout/landingPage";
 
 function App() {
   return (
-    <div className="App">
-    <Sidebar />
-        
+    <div className="App"><div>
+    <Sidebar /></div><div>
+    <LandingPage /></div>
     </div>
   );
 }
--- src/Components/layout/sidebar.js
+++ src/Components/layout/sidebar.js
@@ -1,35 +1,21 @@
 import React from 'react';
 import HomeIcon from '@material-ui/icons/Home';
-import AssignmentIcon from '@material-ui/icons/Assignment';
-import EqualizerIcon from '@material-ui/icons/Equalizer';
-import DateRangeIcon from '@material-ui/icons/DateRange';
-import RestoreIcon from '@material-ui/icons/Restore';
+import SearchIcon from '@material-ui/icons/Search';
+
 
 const Sidebar = () => {
     return (
         <div className="container sidebar">
             
-            <p className="title" style={{backgroundColor:"#12121200", padding:"0px 0 20px 0px"}}>Life Steps</p>
+            <p className="logo" style={{backgroundColor:"#12121200", padding:"0px 0 20px 0px", }}>Finger Notes</p>
             
             <div className="navItem">
                 <HomeIcon className="navIcon"/>
                 <p className="navText">Home</p>
             </div>
             <div className="navItem">
-                <AssignmentIcon className="navIcon"/>
-                <p className="navText">Tasks</p>
-            </div>
-            <div className="navItem">
-                <RestoreIcon className="navIcon"/>
-                <p className="navText">Daily</p>
-            </div>
-            <div className="navItem">
-                <DateRangeIcon className="navIcon"/>
-                <p className="navText">Monthly</p>
-            </div>
-            <div className="navItem">
-                <EqualizerIcon className="navIcon"/>
-                <p className="navText">Performance</p>
+                <SearchIcon className="navIcon"/>
+                <p className="navText">Search</p>
             </div>
         </div> 
     )
