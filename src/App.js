4d3e91a703aa24dc68a2f7117f7874394696e47d
Karanveer-Mul <karanveer7171@gmail.com>
2021-01-18 18:09:33 +0530

    initializing sidebar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import './App.css';
+import Sidebar from "./Components/layout/sidebar";
 
 function App() {
   return (
     <div className="App">
-        <p className="title">
-          This is Title
-        </p>
-        <p className="subHeading">
-          This is subHeading
-        </p>
-        <p className="paraText">
-          This is paraText
-        </p>
+    <Sidebar />
+        
     </div>
   );
 }
---
+++ src/Components/Login/login.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Login = () => {
+
+    return (
+        <div className="container Form">
+            <form>
+                
+            </form>
+        </div>
+    )
+}
+
+export default Login;
+
